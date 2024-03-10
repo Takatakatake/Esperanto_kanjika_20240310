93ba998a0c1130707a562773a8c81043beb79831
Takatakatake <89527567+Takatakatake@users.noreply.github.com>
2024-03-10 17:18:54 +0900

    20240310

--- main.py
+++ main.py
@@ -1,50 +1,124 @@
 
 import streamlit as st
 import re
-import json
-import copy
-import io
-from collections import OrderedDict
 
-with open('sorted_vocab.json', 'r') as f:
-    sorted_vocab = json.load(f, object_pairs_hook=OrderedDict)
-with open('sorted_vocab_copy.json', 'r') as f:
-    sorted_vocab_copy = json.load(f, object_pairs_hook=OrderedDict)
-sorted_keys = list(sorted_vocab.keys())
-
-def replacer(s, new_word):###置換する語根すべてについて小文字か大文字か頭だけ大文字かを確認し、変換後もそれに合わせるための関数。 
-    if s.islower():##それぞれqqq,QQQ,Qqqから始まるunicode化した状態に変化させることで、元の語根の大文字小文字の情報を保存している。　あくまで近似である。 (頭だけ大文字の場合:Qqq&#110;&#117;&#114;)
-        return new_word.lower()
-    elif s.isupper():
-        return new_word.upper()
-    elif s[0].isupper():
-        return new_word.capitalize()
-    else:
-        return new_word
-def multi_smart_replace(text, replacements):##replacerを使って大文字小文字の状態を保ったままの変換
-    # for old_word, new_word in sorted_replacements:
-    for old_word, new_word in replacements.items():
-        text = re.sub(re.escape(old_word), lambda match: replacer(match.group(0), new_word), text, flags=re.IGNORECASE)
-
-    # print(sorted_replacements)
-    return text
-def multiple_replace(text, replace_dict):
-    # 各キーを正規表現のパターンとして結合する
-    pattern = re.compile("|".join([re.escape(k) for k in replace_dict.keys()]), re.M)
-
-    # 置換関数
-    def sub_func(match):
-        return replace_dict[match.group(0)]
-
-    # 置換を実行
-    return pattern.sub(sub_func, text)
-
-esperanto_to_x = {"ĉ": "cx","ĝ": "gx","ĥ": "hx","ĵ": "jx","ŝ": "sx","ŭ": "ux","Ĉ": "Cx","Ĝ": "Gx","Ĥ": "Hx","Ĵ": "Jx","Ŝ": "Sx","Ŭ": "Ux",
-"c^": "cx","g^": "gx","h^": "hx","j^": "jx","s^": "sx","u^": "ux","C^": "Cx","G^": "Gx","H^": "Hx","J^": "Jx","S^": "Sx","U^": "Ux"}
+# 置換用の辞書
+esperanto_to_x = {
+    "ĉ": "cx", "ĝ": "gx", "ĥ": "hx", "ĵ": "jx", "ŝ": "sx", "ŭ": "ux",
+    "Ĉ": "Cx", "Ĝ": "Gx", "Ĥ": "Hx", "Ĵ": "Jx", "Ŝ": "Sx", "Ŭ": "Ux",
+    "c^": "cx", "g^": "gx", "h^": "hx", "j^": "jx", "s^": "sx", "u^": "ux",
+    "C^": "Cx", "G^": "Gx", "H^": "Hx", "J^": "Jx", "S^": "Sx", "U^": "Ux"
+}
 x_to_jijofu={'cx': 'ĉ', 'gx': 'ĝ', 'hx': 'ĥ', 'jx': 'ĵ', 'sx': 'ŝ', 'ux': 'ŭ', 'Cx': 'Ĉ',
              'Gx': 'Ĝ', 'Hx': 'Ĥ', 'Jx': 'Ĵ', 'Sx': 'Ŝ', 'Ux': 'Ŭ'}
 x_to_hat={'cx': 'c^', 'gx': 'g^', 'hx': 'h^', 'jx': 'j^', 'sx': 's^', 'ux': 'u^', 'Cx': 'C^',
           'Gx': 'G^', 'Hx': 'H^', 'Jx': 'J^', 'Sx': 'S^', 'Ux': 'U^'}
+
+def replace_esperanto_chars(text,letter_dictionary):
+    # 各エスペラント文字をX形式に置換
+    for esperanto_char, x_char in letter_dictionary.items():
+        text = text.replace(esperanto_char, x_char)
+    return text
+
+# 置換実行
+# replaced_text = replace_esperanto_chars(text,esperanto_to_x)
+# replaced_text =replace_esperanto_chars(text,x_to_jijofu)
+# replaced_text =replace_esperanto_chars(text,x_to_hat)
+
+with open('No.10000_79999.txt', 'r') as file:
+    loaded_strings = [line.strip() for line in file]
+
+
+hanzi_level=3
+# hanzi_level1=['lv1','形1','形2','形3','形4','形5','数1','数4','接1','前1','代1','頭1','動2','動3','動4','動5','尾1','副1','副2','名2','名3','名4','名5']
+# hanzi_level2=['lv1','lv2','間7','形1','形2','形3','形4','形5','形6','形7','形8','数1','数4','接1','接8','前1','前7','代1','頭1','動2','動3','動4','動5','動6','動7','動8','尾1','副1','副2','副6','副7','副8','名2','名3','名4','名5','名6','名7','名8']
+# hanzi_level3=['lv1','lv2','lv3','間7','間o','擬f','形1','形2','形3','形4','形5','形6','形7','形8','形9','形f','形o','数1','数4','接1','接8','前1','前7','代1','頭1','頭9','動2','動3','動4','動5','動6','動7','動8','動9','動f','動n','動o','尾1','尾9','副1','副2','副6','副7','副8','副9','副b','副f','名2','名3','名4','名5','名6','名7','名8','名9','名b','名f','名i','名n','名o','略f']
+Hanzi_Level=[['lv1','形1','形2','形3','形4','形5','数1','数4','接1','前1','代1','頭1','動2','動3','動4','動5','尾1','副1','副2','名2','名3','名4','名5'],['lv1','lv2','間7','形1','形2','形3','形4','形5','形6','形7','形8','数1','数4','接1','接8','前1','前7','代1','頭1','動2','動3','動4','動5','動6','動7','動8','尾1','副1','副2','副6','副7','副8','名2','名3','名4','名5','名6','名7','名8'],['lv1','lv2','lv3','間7','間o','擬f','形1','形2','形3','形4','形5','形6','形7','形8','形9','形f','形o','数1','数4','接1','接8','前1','前7','代1','頭1','頭9','動2','動3','動4','動5','動6','動7','動8','動9','動f','動n','動o','尾1','尾9','副1','副2','副6','副7','副8','副9','副b','副f','名2','名3','名4','名5','名6','名7','名8','名9','名b','名f','名i','名n','名o','略f']]
+HANZILEVEL=Hanzi_Level[hanzi_level-1]
+
+replacements_dict = {}##単語の追加は辞書式配列でするのが簡単
+with open("全語根＿約11200個.txt", 'r', encoding='utf-8') as file:
+    roots = file.readlines()
+    for root in roots:
+        root = root.strip()
+        if len(root)>4:###5文字以上のみ 調節ポイント
+            replacements_dict[root]=[root,len(root)*10000]##漢字化するかどうかで置換の優先順位が変わるので、漢字化しないものはすべての並べ替えが終わってから、
+            
+import pandas as pd
+# ファイルパスを指定
+file_path = './20240306世界语词根列表_0308.xlsx'
+sheet_name = '确认用于汉字化的汉字（最终仅使用此表格进行世界语文本的汉字化）'
+# 指定されたシートを読み込む
+df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+# 結果を格納するリストを初期化
+# 正確な列名をここで指定
+COLUMN_A_HEADER = '世界语单词'# 実際のA列のヘッダー名に置き換えてください
+COLUMN_C_HEADER = '候选汉字1'# 実際のC列のヘッダー名に置き換えてください
+COLUMN_B_HEADER = '词性与重要性'# 実際のB列のヘッダー名に置き換えてください
+
+# A列とC列のデータをループ処理し、条件に従ってreplacementsリストに追加
+for index, row in df.iterrows():
+    VALUE_A,VALUE_B,VALUE_C=str(row[COLUMN_A_HEADER]),str(row[COLUMN_B_HEADER]),str(row[COLUMN_C_HEADER])
+    # A列の値に"#"が含まれているかチェック
+    if ("#" in VALUE_A) or VALUE_A=='nan' or pd.isna(row[COLUMN_A_HEADER]):
+        continue  # "#"が含まれている場合はスキップ
+    # replacementsリストに(A列の値, C列の値)のタプルとして追加
+    # if row[COLUMN_A_HEADER] not in replacements_dict:
+    #     print(row[COLUMN_A_HEADER])
+    if VALUE_C=='nan' or pd.isna(row[COLUMN_C_HEADER]) or (VALUE_B not in HANZILEVEL):##ここで4文字以下を除く手もある。
+        replacements_dict[VALUE_A]=[VALUE_A,len(VALUE_A)*10000]
+    else:
+        replacements_dict[VALUE_A]=[VALUE_C+VALUE_A+")",len(VALUE_A)*10000+5000]##実際に漢字化するものを優先するため         
+            
+with open('後から加える語根リスト(優先順位も決められる).txt', 'r') as file:
+    for line in file:
+        line = line.rstrip()
+        j = line.split(',')
+        if len(j)==4:
+            word,level,hanzi,priority=j[0],j[1],j[2],j[3]
+            if level in HANZILEVEL:
+                if word==hanzi:
+                    replacements_dict[word]=[hanzi,int(priority)]##一旦整数に変えておく。(どちらでも良い)
+                else:
+                    replacements_dict[word]=[hanzi+word+')',int(priority)]##一旦整数に変えておく。(どちらでも良い)          
+
+
+pre_replacements=[]
+for old,new in replacements_dict.items():
+    pre_replacements.append((old,new[0],new[1]))
+    if (24000 <= new[1] <= 26000) or (34000 <= new[1] <= 36000) or (44000 <= new[1] <= 46000):##調節　実際に漢字化する3,4文字のエスペラント語根のみ
+        for aiueo in list("aiueo"):
+            pre_replacements.append((old+aiueo,new[0]+aiueo,new[1]))
+            
+pre_replacements2=[]
+for old,new,priority in pre_replacements:
+    pre_replacements2.append((old,new,priority))
+    pre_replacements2.append((old.upper(),new.upper(),priority))
+    pre_replacements2.append((old.capitalize(),new.capitalize(),priority))
+
+pre_replacements3 = sorted(pre_replacements2, key=lambda x: x[2], reverse=True)
+
+replacements=[]
+for kk in range(len(pre_replacements3)):
+    replacements.append([pre_replacements3[kk][0],pre_replacements3[kk][1],loaded_strings[kk]])
+    
+def safe_replace(text, replacements):
+    # 必要な置換を記録するための辞書を初期化
+    valid_replacements = {}
+    # テキスト内の各置換対象文字列をチェックし、プレースホルダーに置換
+    for old, new, placeholder in replacements:
+        if old in text:
+            text = text.replace(old, placeholder)
+            # この置換が後で実際に行う必要があることを辞書に記録
+            valid_replacements[placeholder] = new
+    # プレースホルダーを実際の新しい文字列に置換
+    for placeholder, new in valid_replacements.items():
+        text = text.replace(placeholder, new)
+
+    return text
+
+
 
 st.title("エスペラントの漢字化")
 st.caption('これはエスペラント文を漢字表記に変換するアプリです。')
@@ -80,32 +154,24 @@ with st.form(key='profile_form'):
     # ボタン
     submit_btn = st.form_submit_button('送信')
     cancel_btn = st.form_submit_button('キャンセル')
-    if submit_btn:    
-        # process the Esperanto text
-        ## 原文がX型か字上符型か^型かを確認(0はX型、1は字上符型、2は^型)
-
-        text1 = multiple_replace(sentence, esperanto_to_x)
-
-        replacements_1 = {key: sorted_vocab[key][0] for key in sorted_keys}
-        text2 = multi_smart_replace(text1, replacements_1)
-
-        # Build the unicode replacements dictionary
-        replacements_2 = {sorted_vocab_copy[key][0]: key for key in sorted_keys if '&#' in sorted_vocab_copy[key][0]}
-        text3 = multi_smart_replace(text2, replacements_2)
-
-        if letter_type=='字上符':
-            text4 = multiple_replace(text3, x_to_jijofu)
+    if submit_btn:
+        replaced_text = replace_esperanto_chars(sentence,esperanto_to_x)
+        # replaced_text = replace_esperanto_chars(text,esperanto_to_x)
+# replaced_text =replace_esperanto_chars(text,x_to_jijofu)
+# replaced_text =replace_esperanto_chars(text,x_to_hat)
+        text=safe_replace(replaced_text, replacements)
+        if letter_type=='字上符':   
+            text = replace_esperanto_chars(text,x_to_jijofu)
         elif letter_type=='^形式':
-            text4 = multiple_replace(text3, x_to_hat)
-        
-        st.text_area("", text4, height=300)
+            text = replace_esperanto_chars(text,x_to_hat)
+        st.text_area("", text, height=300)
           
         
 to_download = io.BytesIO(text4.encode())
 st.download_button(
 label="テキストをダウンロード",
 data=to_download,
-file_name="processed_text.html",
+file_name="processed_text.txt",
 mime="text/plain"
 )
     
