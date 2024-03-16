f3c846cc32247e62c6b3bb2f7ba96c4a68798237
Takatakatake <89527567+Takatakatake@users.noreply.github.com>
2024-03-16 14:59:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -25,77 +25,24 @@ def replace_esperanto_chars(text,letter_dictionary):
 # replaced_text =replace_esperanto_chars(text,x_to_jijofu)
 # replaced_text =replace_esperanto_chars(text,x_to_hat)
 
-with open('No.10000_79999.txt', 'r') as file:
+with open('No.10000_500000.txt', 'r') as file:
     loaded_strings = [line.strip() for line in file]
 
 
-hanzi_level=3
-# hanzi_level1=['lv1','形1','形2','形3','形4','形5','数1','数4','接1','前1','代1','頭1','動2','動3','動4','動5','尾1','副1','副2','名2','名3','名4','名5']
-# hanzi_level2=['lv1','lv2','間7','形1','形2','形3','形4','形5','形6','形7','形8','数1','数4','接1','接8','前1','前7','代1','頭1','動2','動3','動4','動5','動6','動7','動8','尾1','副1','副2','副6','副7','副8','名2','名3','名4','名5','名6','名7','名8']
-# hanzi_level3=['lv1','lv2','lv3','間7','間o','擬f','形1','形2','形3','形4','形5','形6','形7','形8','形9','形f','形o','数1','数4','接1','接8','前1','前7','代1','頭1','頭9','動2','動3','動4','動5','動6','動7','動8','動9','動f','動n','動o','尾1','尾9','副1','副2','副6','副7','副8','副9','副b','副f','名2','名3','名4','名5','名6','名7','名8','名9','名b','名f','名i','名n','名o','略f']
-Hanzi_Level=[['lv1','形1','形2','形3','形4','形5','数1','数4','接1','前1','代1','頭1','動2','動3','動4','動5','尾1','副1','副2','名2','名3','名4','名5'],['lv1','lv2','間7','形1','形2','形3','形4','形5','形6','形7','形8','数1','数4','接1','接8','前1','前7','代1','頭1','動2','動3','動4','動5','動6','動7','動8','尾1','副1','副2','副6','副7','副8','名2','名3','名4','名5','名6','名7','名8'],['lv1','lv2','lv3','間7','間o','擬f','形1','形2','形3','形4','形5','形6','形7','形8','形9','形f','形o','数1','数4','接1','接8','前1','前7','代1','頭1','頭9','動2','動3','動4','動5','動6','動7','動8','動9','動f','動n','動o','尾1','尾9','副1','副2','副6','副7','副8','副9','副b','副f','名2','名3','名4','名5','名6','名7','名8','名9','名b','名f','名i','名n','名o','略f']]
-HANZILEVEL=Hanzi_Level[hanzi_level-1]
-
-replacements_dict = {}##単語の追加は辞書式配列でするのが簡単
-with open("全語根＿約11200個.txt", 'r', encoding='utf-8') as file:
-    roots = file.readlines()
-    for root in roots:
-        root = root.strip()
-        if len(root)>4:###5文字以上のみ 調節ポイント  malm,avis,deviなど色々あるので
-            replacements_dict[root]=[root,len(root)*10000]##漢字化するかどうかで置換の優先順位が変わるので、漢字化しないものはすべての並べ替えが終わってから、
-            
-
-with open('./20240306世界语词根列表_0308.csv', 'r') as file:
+replacements3=[]
+with open("replacements2_list.txt", 'r', encoding='utf-8') as file:
     for line in file:
         line = line.strip()
         j = line.split(',')
-        if len(j)==4:
-            word,level,hanzi=j[0],j[1],j[2]
-            if ("#" in word) or word=='':
-                continue
-            elif hanzi=='' or (level not in HANZILEVEL):
-                replacements_dict[word]=[word,len(word)*10000]##ここで4文字以下を除く手もある。
-            else:
-                replacements_dict[word]=[hanzi+'('+word+")",len(word)*10000+5000]##実際に漢字化するものを優先するため        
-            
-with open('後から加える語根リスト(優先順位も決められる).txt', 'r') as file:
-    for line in file:
-        line = line.strip()
-        j = line.split(',')
-        if len(j)==4:
-            word,level,hanzi,priority=j[0],j[1],j[2],j[3]
-            if level in HANZILEVEL:
-                if word==hanzi:
-                    replacements_dict[word]=[hanzi,int(priority)]##一旦整数に変えておく。(どちらでも良い)
-                else:
-                    replacements_dict[word]=[hanzi+'('+word+')',int(priority)]##一旦整数に変えておく。(どちらでも良い)          
-
-root_de_fusiyo=['min','amas','mas','vin','boj','tas']
-pre_replacements=[]
-for old,new in replacements_dict.items():
-    if old not in root_de_fusiyo:
-        pre_replacements.append((old,new[0],new[1]))
-    if (24000 <= new[1] <= 26000) or (34000 <= new[1] <= 36000) or (44000 <= new[1] <= 46000):##調節　実際に漢字化する3,4文字のエスペラント語根のみ
-        for aiueo in list("aiueo"):
-            pre_replacements.append((old+aiueo,new[0]+aiueo,new[1]))##漢字化の守備範囲を広げるため
-            
-pre_replacements2=[]
-for old,new,priority in pre_replacements:##大文字のみ、小文字のみ、頭文字だけ大文字の単語に対応させるため。
-    pre_replacements2.append((old,new,priority))
-    pre_replacements2.append((old.upper(),new.upper(),priority))
-    pre_replacements2.append((old.capitalize(),new.capitalize(),priority))
-
-pre_replacements3 = sorted(pre_replacements2, key=lambda x: x[2], reverse=True)
-
-replacements=[]
-for kk in range(len(pre_replacements3)):
-    replacements.append([pre_replacements3[kk][0],pre_replacements3[kk][1],loaded_strings[kk]])
+        if len(j)==3:
+            old,new,place_holder=j[0],j[1],j[2]
+            replacements3.append((old,new,place_holder))
     
-def safe_replace(text, replacements):
+def safe_replace(text, replacements3):
     # 必要な置換を記録するための辞書を初期化
     valid_replacements = {}
     # テキスト内の各置換対象文字列をチェックし、プレースホルダーに置換
-    for old, new, placeholder in replacements:
+    for old, new, placeholder in replacements3:
         if old in text:
             text = text.replace(old, placeholder)
             # この置換が後で実際に行う必要があることを辞書に記録
@@ -111,7 +58,10 @@ def safe_replace(text, replacements):
 st.title("世界语汉字化")
 st.caption('这是一个将世界语文本转换成汉字符号的网络应用程序。')
 
-
+code = '''
+import streamlit as st
+'''
+st.code(code,language='python')
 
 
 
@@ -141,7 +91,7 @@ with st.form(key='profile_form'):
         # replaced_text = replace_esperanto_chars(text,esperanto_to_x)
 # replaced_text =replace_esperanto_chars(text,x_to_jijofu)
 # replaced_text =replace_esperanto_chars(text,x_to_hat)
-        text=safe_replace(replaced_text, replacements)
+        text=safe_replace(replaced_text, replacements3)
         if letter_type=='字上符':   
             text = replace_esperanto_chars(text,x_to_jijofu)
         elif letter_type=='^形式':
